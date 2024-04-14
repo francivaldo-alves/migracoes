52a3c4ff7ff3b573204c7afd595d5c0cb6ec6f9e
Francivaldo Alves de Souza <francivaldo.alves@gmail.com>
2024-04-14 18:06:54 -0300

    Add files via upload
    Projeto de migração de terminais TDS

---
+++ script.js
@@ -0,0 +1,48 @@
+let totalMigracao = 100;
+let meta = 2000;
+let dataUltimaAtualizacao = "15/04/2024"
+
+document.querySelector("#data").innerHTML = " Terminais migrados até: " + dataUltimaAtualizacao
+document.querySelector("#numero").innerHTML = totalMigracao
+let numero = totalMigracao;
+
+// Calcula a porcentagem de progresso
+let porcentagem = (numero / meta) * 100;
+let porcentagemFormatada = porcentagem.toFixed(2);
+
+
+// Obtendo a barra de progresso e a mensagem
+const progressBar = document.getElementById('progressBar');
+const message = document.getElementById('message');
+
+// Definindo a largura da barra de progresso e a mensagem com base na porcentagem
+progressBar.style.width = porcentagemFormatada + '%';
+progressBar.setAttribute('aria-valuenow', porcentagemFormatada);
+message.textContent = ` ${porcentagemFormatada}%`;
+
+
+// Definindo a cor da barra de progresso e a mensagem com base na porcentagem
+if (porcentagemFormatada <= 84.99) {
+  progressBar.classList.add('bg-warning');
+  message.textContent += ' - migrados!';
+
+} else if (porcentagemFormatada <= 89.99) {
+  progressBar.classList.add('bg-info');
+  message.textContent += ' - migrados!';
+
+} else if (porcentagemFormatada <= 94.99) {
+
+  progressBar.classList.add('bg-primary');
+  message.textContent += ' - migrados!';
+
+} else {
+  progressBar.classList.add('bg-success');
+  message.textContent += ' - Migrados!';
+}
+
+setTimeout(() => {
+  document.location.reload();
+}, 180000);
+
+
+
