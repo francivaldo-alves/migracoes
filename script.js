93b4cc432dfd4d35d556d2db429531c677bb3ace
Francivaldo Alves de Souza <francivaldo.alves@gmail.com>
2024-09-04 17:04:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let totalMigracao = 3240;
+let totalMigracao = 3412;
 let meta = 7935;
-let dataUltimaAtualizacao = "31/08/2024"
+let dataUltimaAtualizacao = "03/09/2024"
 
 document.querySelector("#data").innerHTML = " Terminais migrados até: " + dataUltimaAtualizacao
 document.querySelector("#numero").innerHTML = totalMigracao
